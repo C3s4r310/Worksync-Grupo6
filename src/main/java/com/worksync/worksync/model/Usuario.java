95c551439c12ec6034c6ddf0e926e60dc673eae7
C3s4r310 <cu046481@gmail.com>
2026-05-28 12:11:24 -0500

    feat: Agregar entidad usuario

---
+++ src/main/java/com/worksync/worksync/model/Usuario.java
@@ -0,0 +1,63 @@
+package com.worksync.worksync.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @Column(unique = true)
+    private String correo;
+
+    private String contrasena;
+
+    private String rol;
+
+    // Constructor vacío
+    public Usuario() {
+    }
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
