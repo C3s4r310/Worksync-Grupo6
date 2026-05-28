9f71761ea17c989dce315877cff0ecdde4b43154
C3s4r310 <cu046481@gmail.com>
2026-05-28 13:00:49 -0500

    feat: JWT

--- src/main/java/com/worksync/worksync/JWT/JwtAuthenticationFilter.java
+++ src/main/java/com/worksync/worksync/JWT/JwtAuthenticationFilter.java
@@ -1,5 +1,28 @@
 package com.worksync.worksync.JWT;
 
-public class JwtAuthenticationFilter {
-    
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final String SECRET =
+            "worksyncsecretkeyworksyncsecretkey";
+
+    private static final Key KEY =
+            Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public static String generarToken(String correo){
+
+        return Jwts.builder()
+                .setSubject(correo)
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + 86400000)
+                )
+                .signWith(KEY)
+                .compact();
+    }
 }
