ba1509820affb521ef2c47f87a0511eaceafa7dc
Jeremy Gutarra <jeremybenjamin.gp@gmail.com>
2026-05-28 22:50:32 -0500

    configurado CORS para aceptar peticiones desde localhost:5173

--- src/main/java/com/worksync/worksync/Config/WebConfig.java
+++ src/main/java/com/worksync/worksync/Config/WebConfig.java
@@ -1,5 +1,20 @@
 package com.worksync.worksync.Config;
 
-public class WebConfig {
-    
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+
 }
